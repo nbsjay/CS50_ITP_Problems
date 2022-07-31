ab913b27bbba8f3d615d2e22afbcfe91ab714d88
Nathan <97744551+nbsjay@users.noreply.github.com>
2022-07-31 15:49:44 +0000

    Added the question

--- 0/indoor.py
+++ 0/indoor.py
@@ -1,3 +1,9 @@
+"""
+In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase.
+Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user explicitly,
+as by passing a str of your own as an argument to input
+"""
+
 def main():
     # ask for the user's input
     x = input("type something here ")
