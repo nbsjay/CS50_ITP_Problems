fc15f29562f84b8e743b8b925dd81248b139bf67
Nathan <97744551+nbsjay@users.noreply.github.com>
2022-07-31 15:48:20 +0000

    Create playback.py

---
+++ 0/playback.py
@@ -0,0 +1,18 @@
+"""
+In a file called indoor.py, implement a program in Python that prompts the user for input and then outputs that same input in lowercase.
+Punctuation and whitespace should be outputted unchanged. You’re welcome, but not required, to prompt the user explicitly,
+as by passing a str of your own as an argument to input
+"""
+
+
+def main():
+    x = input("type something here ")
+    slow(x)
+
+
+def slow(ask):
+    print(ask.replace(" ", "..."))
+    # replace function from python's documentation
+
+
+main()
