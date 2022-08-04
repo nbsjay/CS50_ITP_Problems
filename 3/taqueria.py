2b0b7e11c9f1c30dafdf872e1abd86b556917df4
nbsjay <nathanbrownsmith@outlook.com>
2022-08-04 15:02:25 +0000

    added an important note

--- 3/taqueria.py
+++ 3/taqueria.py
@@ -1,3 +1,12 @@
+"""
+In a file called taqueria.py, implement a program that enables a user to place an order,
+prompting them for items, one per line, until the user inputs control-d (which is a common way of ending one’s input to a program).
+After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($) and formatted to two decimal places.
+Treat the user's input case insensitively. Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
+"""
+
+
+
 menu = {
     "Baja Taco": 4.00,
     "Burrito": 7.50,
@@ -35,3 +44,12 @@ while True:
     except EOFError:
         print("\n")
         break
+
+
+"""
+An important note:
+
+David failed to mention that windows users do cannot use 'control d' to exit out of a running code.
+becuase of this the exception will not catch the keyboard combination
+if you are working on a locally installed vscode, rather use 'control z'
+"""
